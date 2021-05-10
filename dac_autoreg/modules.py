b246419b4d0287a2724b83ae9c7c7352c0c4f86d
Turall <tural.muradov@pashabank.az>
2021-05-10 20:36:11 +0400

    fix endpoint check

--- dac_autoreg/modules.py
+++ dac_autoreg/modules.py
@@ -1,11 +1,11 @@
 from http import HTTPStatus
 from typing import Union, Set, List
-import asyncio
 import httpx
 import logging
 
+
 class Autoreg:
-    def __init__(self, app, settings: object, log: object= None,exclude_list: Union[Set[str],List[str]] = None):
+    def __init__(self, app, settings: object, log: object = None, exclude_list: Union[Set[str], List[str]] = None):
         """[Auto registration endpoints in DAC]
         Args:
             app: [Instance of Fastapi app]
@@ -15,12 +15,14 @@ class Autoreg:
         self.app = app
         self.log = log or logging.getLogger(__name__)
         # endpoints in exclude_list is not registered in DAC
-        self.exclude_list = {"/openapi.json", "/docs", "/docs/oauth2-redirect", "/redoc", "/metrics/"}
+        self.exclude_list = {"/openapi.json", "/docs",
+                             "/docs/oauth2-redirect", "/redoc", "/metrics/"}
         self.dac_url = settings.DAC_URL
         self.service_name = settings.SERVICE_NAME
         self.prefixes = set()
         if exclude_list:
-            self.exclude_list = self.exclude_list | exclude_list if isinstance(exclude_list,set) else set(exclude_list)
+            self.exclude_list = self.exclude_list | exclude_list if isinstance(
+                exclude_list, set) else set(exclude_list)
 
     async def autoreg(self):
         """
@@ -34,7 +36,6 @@ class Autoreg:
                     self.prefixes.add(route.path)
                 except Exception as err:
                     self.log.error(err, exc_info=True)
-  
 
         # Creating service
         service_id = await self.create_service()
@@ -43,8 +44,7 @@ class Autoreg:
         if service_id:
             await self.create_endpoints(service_id)
 
-
-    async def create_endpoints(self,service_id: str):
+    async def create_endpoints(self, service_id: str):
         """[summary]
         Args:
             service_id (str): [service_id of created service]
@@ -55,29 +55,29 @@ class Autoreg:
         # We will use this prefixes list to check if endpoint is registered or not
         registered_prefixes = await self.registered_endpoints()
 
-        for prefix in registered_prefixes:
+        for prefix in self.prefixes:
             if prefix not in registered_prefixes:
                 async with httpx.AsyncClient() as client:
                     await client.post(
-                        f"{self.dac_url}/endpoints", 
+                        f"{self.dac_url}/endpoints",
                         json={"service_id": service_id, "prefix": prefix}
-                        )
-                    
+                    )
+
                     self.log.info(f"{prefix} registered")
             else:
-                log.warning(f"{prefix} is already registered")
+                self.log.warning(f"{prefix} is already registered")
 
     async def create_service(self):
         """[summary]
 
         Returns:
             [type]: [service id (str)]
-        
+
         Flow:
             [get service_id] if exists return service_id
             [create service_id] if service not exists
         """
-        # In one async with client we can not send 2 requests. 
+        # In one async with client we can not send 2 requests.
 
         # Get service_name from database. Return id if exists
         try:
@@ -86,16 +86,15 @@ class Autoreg:
                 if response.status_code == HTTPStatus.OK:
                     self.log.info(f"{response.json().get('name')} exists")
                     return response.json().get("id")
-            
+
             # Create service_name if not exists
             async with httpx.AsyncClient() as client:
-                response = await client.post(f"{self.dac_url}/services", json = {"name": self.service_name})
+                response = await client.post(f"{self.dac_url}/services", json={"name": self.service_name})
                 self.log.info(f"{response.json().get('name')} created")
                 if response:
                     return response.json().get("id")
         except Exception as err:
-            self.log.error(err,exc_info=True)
-
+            self.log.error(err, exc_info=True)
 
     async def registered_endpoints(self):
         """Get registered endpoints from database
@@ -104,7 +103,9 @@ class Autoreg:
             [list]: [Getting prefixes from endpoints registered in database]
         """
         async with httpx.AsyncClient() as client:
-                    endpoints = await client.get(
-                        f"{self.dac_url}/endpoints"
-                    )
-        return [content.get("prefix") for content in endpoints.json()]
+            endpoints = await client.get(
+                f"{self.dac_url}/endpoints"
+            )
+            if endpoints.status_code == 200:
+                return [content.get("prefix") for content in endpoints.json()]
+            return []
